9b4a2790fca8e48f05e14bf523e695235281bcd9
limseongkyu <96x60812@naver.com>
2021-04-23 23:59:45 +0900

    [Add] google login

--- src/main/java/com/loopy/web/IndexController.java
+++ src/main/java/com/loopy/web/IndexController.java
@@ -1,15 +1,30 @@
 package com.loopy.web;
 
+import com.loopy.config.auth.dto.SessionUser;
+import com.loopy.domain.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 
 //mustache는 경로 자동으로 지정됨. view resolver가 자동 처리.
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        SessionUser user = (User) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 }
