51ede7170919b04e8778753d876509b03c160b51
limseongkyu <96x60812@naver.com>
2021-06-15 22:25:37 +0900

    [Fix] post print 게시판에서 글을 출력 시 모두 출력이 안되는 문제 해결.

--- src/main/java/com/loopy/Application.java
+++ src/main/java/com/loopy/Application.java
@@ -2,7 +2,9 @@ package com.loopy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //jpa auditing 활성화
 //메인 클래스.
 // 스프링부트 자동 설정, BEAN 읽기 및 생성 자동화.
 @SpringBootApplication
